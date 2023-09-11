90b1845cea0a9ffa998a24eb691505bbfcf17c5c
1isaackarani <125805240+1isaackarani@users.noreply.github.com>
2023-09-11 08:35:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -42,31 +42,32 @@ let wibh = moment.tz('Asia/Karachi').format('HH')
 
 // if you don't have it, type "-" or leave it alone so that there are credits :v
 global.sig = 'https://instagram.com/itx_ahmad.ali' //Link Instagrammu
-global.sgh = 'https://github.com/xIKRATOSx' //Link Githubmu
+global.sgh = 'https://github.com/1isaackarani' //Link Githubmu
 global.sgc = 'https://chat.whatsapp.com/DxIb5oWzpg24mmg9D9zw5Y'
 global.sdc = '-' //Fill it out using the Discord link, if there is one, just leave it
 global.snh = 'https://nhentai.net/g/HaramTod🗿' //Hmmm, This is up to you to fill in :v
 
 /*============== PAYMENT ==============*/
-global.pdana = '085736178354' //Nomor Danamu
-global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
+global.pdana = '254768495675' //Nomor Danamu
+global.ppulsa = '254768495675' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '68673045381' //Nomor Bot
+global.nomorbot = '254768495675' //Nomor Bot
 global.nomorown = '923470027813' //Nomor Owner
-global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.namebot = 'Isᴀᴀᴄ 부스팅☢︎︎MY💯 아드레날린 🤬||ᵗʳᵉᵐᵉⁿᵈᵒ||ᴀᴅʀᴇɴᴀʟɪɴᴇ' //Nama Bot
+global.nameown = 'Isᴀᴀᴄ 부스팅☢︎︎MY💯 아드레날린 🤬||ᵗʳᵉᵐᵉⁿᵈᵒ||ᴀᴅʀᴇɴᴀʟɪɴᴇ' // Nama Owner
 
 
 /*============== CHATGPT ==============*/
-global.openAiapi = 'PASTE_YOUR_API_HERE'   // Get you Api-key from 'https://platform.openai.com/account/api-keys'
+global.openAiapi = '4vcRm5VkiXmkn6PXtQbYT3BlbkFJohpZiRei5kOT8pHQvWFq'   // Get you Api-key from 'https://platform.openai.com/account/api-keys'
 global.org = 'PASTE_YOUR_ORG_HERE'   // Get you Org-Key from 'https://platform.openai.com/account/org-settings' 
 
 /*============== STAFF ==============*/
 global.owner = [
   ['923470027813', 'ᴀʜᴍᴀᴅ ᴀʟɪ🎐', true] //don't change this name and number
-  ['19293514545', 'ᴀʜᴍᴀᴅ ᴀʟɪ🎐', false] //Replace your name and number here
+  ['254768495675', '[8:24 AM, 9/11/2023] 🄸𝔰𝔞𝔞𝔠  🅰︎𝔡𝔯𝔢𝔫𝔞𝔩𝔦𝔫𝔢,: Isᴀᴀᴄ ᴀᴅʀᴇɴᴀʟɪɴᴇ
+[8:25 AM, 9/11/2023] 🄸𝔰𝔞𝔞𝔠  🅰︎𝔡𝔯𝔢𝔫𝔞𝔩𝔦𝔫𝔢,: Isᴀᴀᴄ 부스팅☢︎︎MY💯 아드레날린 🤬||ᵗʳᵉᵐᵉⁿᵈᵒ||ᴀᴅʀᴇɴᴀʟɪɴᴇ', false] //Replace your name and number here
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = ['923470027813',''] //Don't replace this,If you want to add someone add any dev
@@ -106,15 +107,17 @@ global.APIKeys = { // APIKey Here
 
 /*============== WATERMARK ==============*/
 global.wm = '「 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Ahmad Ali- ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = 'ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷'
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Isᴀᴀᴄ 부스팅☢︎︎MY💯 아드레날린 🤬||ᵗʳᵉᵐᵉⁿᵈᵒ||ᴀᴅʀᴇɴᴀʟɪɴᴇ- ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '[8:24 AM, 9/11/2023] 🄸𝔰𝔞𝔞𝔠  🅰︎𝔡𝔯𝔢𝔫𝔞𝔩𝔦𝔫𝔢,: Isᴀᴀᴄ ᴀᴅʀᴇɴᴀʟɪɴᴇ
+[8:25 AM, 9/11/2023] 🄸𝔰𝔞𝔞𝔠  🅰︎𝔡𝔯𝔢𝔫𝔞𝔩𝔦𝔫𝔢,: Isᴀᴀᴄ 부스팅☢︎︎MY💯 아드레날린 🤬||ᵗʳᵉᵐᵉⁿᵈᵒ||ᴀᴅʀᴇɴᴀʟɪɴᴇ'
 global.footerTXT = `ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ1 • ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ\nowner: +92 347 0027813`
 global.fig = `Follow me on instagram:\n\t\tinstagram.com/itx_ahmad.ali`
 global.botdate = `⫹⫺ Date: ${week} ${date}`
 global.bottime = `Time : ${wktuwib}`
-global.titlebot = '🎋 ┊ ɪᴋʀᴀᴛᴏs ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ'
+global.titlebot = '🎋 ┊ [8:24 AM, 9/11/2023] 🄸𝔰𝔞𝔞𝔠  🅰︎𝔡𝔯𝔢𝔫𝔞𝔩𝔦𝔫𝔢,: Isᴀᴀᴄ ᴀᴅʀᴇɴᴀʟɪɴᴇ
+[8:25 AM, 9/11/2023] 🄸𝔰𝔞𝔞𝔠  🅰︎𝔡𝔯𝔢𝔫𝔞𝔩𝔦𝔫𝔢,: Isᴀᴀᴄ 부스팅☢︎︎MY💯 아드레날린 🤬||ᵗʳᵉᵐᵉⁿᵈᵒ||ᴀᴅʀᴇɴᴀʟɪɴᴇ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ'
 global.author = global.wm
-global.logo = 'https://i.ibb.co/cc8jfkX/01.jpg'
+global.logo = 'https://telegra.ph/file/7837e3725aaa9f6fb40c4.jpg'
 
 
 
